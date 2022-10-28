c66640e6c39e3f83939dbe6d8e282e6045fcb2cb
Pranav Sharma <88233021+Cyberpunkpranav@users.noreply.github.com>
2022-10-28 14:55:57 +0530

    dashboard

--- src/js/feature.js
+++ src/js/feature.js
@@ -1,13 +1,14 @@
 
 function dragElement(elmnt) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-    elmnt.onmouseover = dragMouseDown;
+    elmnt.onclick = dragMouseDown;
   
   function dragMouseDown(e) {
     e = e || window.event;
     e.preventDefault();
     pos3 = e.clientX;
     pos4 = e.clientY;
+    document.ondblclick = closeDragElement;
     document.onmouseup = closeDragElement;
     document.onmousemove = elementDrag;
   }
